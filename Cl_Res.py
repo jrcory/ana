2b841d3d6b3af4c7d9db66ec0d2af9f3ee35793f
Jordan Cory <cory@frib.msu.edu>
2024-03-13 17:50:01 -0400

    new file to look at result of cluster

---
+++ Cl_Res.py
@@ -0,0 +1,10 @@
+import numpy as np 
+import scipy 
+import pandas as pd
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_pdf import PdfPages
+from sklearn import model_selection
+from sklearn import svm
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
+from matplotlib.patches import Ellipse
